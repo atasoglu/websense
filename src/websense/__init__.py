c2c0bbb7cb730d07dd524fdf7ce3b4daebf78e96
atasoglu <ahmetatasoglu98@gmail.com>
2026-01-27 22:51:56 +0300

    chore(project): update author information and homepage URL
    * Changed author name and email in pyproject.toml
    * Updated homepage URL to reflect the correct GitHub repository
    * Removed unused imports and cleaned up __init__.py

--- src/websense/__init__.py
+++ src/websense/__init__.py
@@ -1,6 +1,3 @@
 from .scraper import Scraper
-from .fetcher import Fetcher
-from .cleaner import Cleaner
-from .parser import Parser
 
-__all__ = ["Scraper", "Fetcher", "Cleaner", "Parser"]
+__all__ = ["Scraper"]
